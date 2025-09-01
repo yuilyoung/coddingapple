32d8f1f9f8a4d1a8086db3acf0f93f6551375b5e
unknown <sdd7878@gmail.com>
2025-09-01 18:03:52 +0900

    first commit

---
+++ forum/app/page.js
@@ -0,0 +1,20 @@
+import connectDB from '@/util/database';
+import { MongoClient } from 'mongodb';
+
+
+export default async function Home() {
+
+  // const url = 'mongodb+srv://sdd7878:!doubt1457@cluster0.0qcp42q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  // const options = { useNewUrlParser: true }
+  // let connectDB = new MongoClient(url).connect();
+
+  let client = await connectDB;
+  const database = client.db('forum');
+  let result = await database.collection('post').find().toArray();
+
+  return (
+    <div className="">
+      {result[0].title}
+    </div>
+  );
+}
---
+++ forum/util/database.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb'
+const url = 'mongodb+srv://sdd7878:!doubt1457@cluster0.0qcp42q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+
+export default connectDB;
